2a0754e5fae9d798fc52d661de1873b2598a9e61
JK-8989 <jiyoungkim365@gmail.com>
2022-06-07 14:59:33 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # parallax-0
+
+clone coding : Online Tutorials
+https://www.youtube.com/watch?v=TawH-AqHTXc
+---
+I kinda messed up with axis Y - need to work on more
---
+++ script.js
@@ -0,0 +1,15 @@
+let nightSky = document.querySelector('img:nth-child(1)');
+let moon = document.querySelector('img:nth-child(2)');
+let mountain = document.querySelector('img:nth-child(3)');
+let road = document.querySelector('img:nth-child(4)');
+let text = document.querySelector('h1');
+
+window.addEventListener('scroll', function(){
+  let value = window.scrollY;
+
+  nightSky.style.top = value * 0.5 + 'px';
+  moon.style.right = value * 0.15 + 'px';
+  mountain.style.top = value * 2 + 'px';
+  road.style.top = value * 0.5 + 'px';
+  text.style.top = value * -1 + 'px';
+})
